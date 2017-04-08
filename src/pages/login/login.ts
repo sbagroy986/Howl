5fcc14456e77d09e34b361303abe2ac102af7db6
sbagroy986 <shrey14099@iiitd.ac.in>
2017-04-08 07:47:11 +0530

    Login page

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Facebook, NativeStorage } from 'ionic-native';
+import { TabsPage } from '../pages/tabs/tabs';
 
 /*
   Generated class for the Login page.
@@ -12,11 +14,42 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  FB_APP_ID: number = 661959977307690;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  	Facebook.browserInit(this.FB_APP_ID, "v2.8");
   }
 
+  doFbLogin(){
+    let permissions = new Array();
+    let nav = this.navCtrl;
+    //the permissions your facebook app needs from the user
+    permissions = ["public_profile"];
+
+
+    Facebook.login(permissions)
+    .then(function(response){
+      let userId = response.authResponse.userID;
+      let params = new Array();
+
+      //Getting name and gender properties
+      Facebook.api("/me?fields=name,gender", params)
+      .then(function(user) {
+        user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+        //now we have the users info, let's save it in the NativeStorage
+        NativeStorage.setItem('user',
+        {
+          name: user.name,
+          gender: user.gender,
+          picture: user.picture
+        })
+        .then(function(){
+          nav.push(TabsPage);
+        }, function (error) {
+          console.log(error);
+        })
+      })
+    }, function(error){
+      console.log(error);
+    });
+  }
 }
