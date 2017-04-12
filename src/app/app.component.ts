dc2765fcce8bad45cdb48813660172069465e6f2
sbagroy986 <shrey14099@iiitd.ac.in>
2017-04-12 19:35:07 +0530

    Rolling back on last commit changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,18 +19,19 @@ export class MyApp {
       // Here we will check if the user is already logged in
       // because we don't want to ask users to log in each time they open the app
       let env = this;
-      // NativeStorage.getItem('auth_user')
-      // .then( function (data) {
-      //   // user is previously logged and we have his data
-      //   // we will let him access the app
-      //   env.nav.setRoot(TabsPage);
-      //   Splashscreen.hide();
-      // }, function (error) {
-        //we don't have the user data so we will ask him to log in
+      NativeStorage.getItem('auth_user')
+      .then( function (data) {
+        // user is previously logged and we have his data
+        // we will let him access the app
         env.nav.setRoot(TabsPage);
         Splashscreen.hide();
-      StatusBar.styleDefault();
+      }, function (error) {
+        //we don't have the user data so we will ask him to log in
+        env.nav.setRoot(LoginPage);
+        Splashscreen.hide();
       });
 
-    }
+      StatusBar.styleDefault();
+    });
+  }
 }
